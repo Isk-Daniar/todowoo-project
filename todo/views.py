219119bf37b245fc0d8261c620e9cd411315362b
Isk-Daniar <109099588+Isk-Daniar@users.noreply.github.com>
2022-10-11 11:37:41 +0500

    Viewing, updating, completing, and deleting tasks

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
 from .models import Todo
 from .forms import TodoForm
+from django.utils import timezone
 
 
 def home(request):
@@ -14,7 +15,7 @@ def home(request):
 
 def signupuser(request):
     if request.method == 'GET':
-            return render(request, 'todo/signupuser.html', {'from':UserCreationForm()})
+            return render(request, 'todo/signupuser.html', {'form':UserCreationForm()})
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
@@ -24,9 +25,9 @@ def signupuser(request):
                 return redirect('currenttodos')
             except IntegrityError:
                 return render(request, 'todo/signupuser.html',
-                              {'from': UserCreationForm(), 'error': 'That username has already been taken. Please choosea new username'})
+                              {'form': UserCreationForm(), 'error': 'That username has already been taken. Please choosea new username'})
         else:
-            return render(request, 'todo/signupuser.html', {'from': UserCreationForm(), 'error':'Password did not match'})
+            return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error':'Password did not match'})
 
 def loginuser(request):
     if request.method == 'GET':
@@ -34,7 +35,7 @@ def loginuser(request):
     else:
         user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
         if user is None:
-            return render(request, 'todo/loginuser.html', {'from':AuthenticationForm(), 'error':'Username and password did not match'})
+            return render(request, 'todo/loginuser.html', {'form':AuthenticationForm(), 'error':'Username and password did not match'})
         else:
             login(request, user)
             return redirect('currenttodos')
@@ -47,7 +48,7 @@ def logoutuser(request):
 
 def createtodo(request):
     if request.method == 'GET':
-        return render(request, 'todo/createtodo.html', {'from':TodoForm()})
+        return render(request, 'todo/createtodo.html', {'form':TodoForm()})
     else:
         try:
             form = TodoForm(request.POST)
@@ -56,9 +57,34 @@ def createtodo(request):
             newtodo.save()
             return redirect('currenttodos')
         except ValueError:
-            render(request, 'todo/createtodo.html', {'from': TodoForm(), 'error':'Bad data passed in. Try again.'})
+            render(request, 'todo/createtodo.html', {'form': TodoForm(), 'error':'Bad data passed in. Try again.'})
 
 def currenttodos(request):
     todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, 'todo/currenttodos.html', {'todos':todos})
 
+def viewtodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'GET':
+        form = TodoForm(instance=todo)
+        return render(request, 'todo/viewtodo.html', {'todo':todo, 'form':form})
+    else:
+        try:
+            form = TodoForm(request.POST, instance=todo)
+            form.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todo/viewtodo.html', {'todo':todo, 'form':form, 'error':'Bad info'})
+
+def completetodo(request, todo_pk):
+            todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+            if request.method == 'GET':
+                todo.datecompleted = timezone.now()
+                todo.save()
+                return redirect('currenttodos')
+
+def deletetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'GET':
+        todo.delete()
+        return redirect('currenttodos')
